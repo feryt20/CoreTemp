70431ab580899b5c828c98a87e45a69718513178
farshad kalhor <farshadt20@yahoo.com>
2021-01-06 22:38:02 +0330

    save

--- CoreTemp.MvcApi/Startup.cs
+++ CoreTemp.MvcApi/Startup.cs
@@ -62,32 +62,5 @@ namespace CoreTemp.MvcApi
             });
         }
 
-        //public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
-        //{
-        //    if (env.IsDevelopment())
-        //    {
-        //        app.UseDeveloperExceptionPage();
-        //    }
-        //    else
-        //    {
-        //        app.UseExceptionHandler("/Home/Error");
-        //        // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-        //        app.UseHsts();
-        //    }
-        //    app.UseHttpsRedirection();
-        //    app.UseStaticFiles();
-
-        //    app.UseRouting();
-
-        //    app.UseAuthentication();
-        //    app.UseAuthorization();
-
-        //    app.UseEndpoints(endpoints =>
-        //    {
-        //        endpoints.MapControllerRoute(
-        //            name: "default",
-        //            pattern: "{controller=Home}/{action=Index}/{id?}");
-        //    });
-        //}
     }
 }
